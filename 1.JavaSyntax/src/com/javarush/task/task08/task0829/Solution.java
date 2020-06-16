a093a7f773300851cdce3fbbcd1add1864f0776f
Dmitry <varbandim@yandex.ru>
2020-06-16 12:55:13 +0300

    Повторение обучения

--- 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -3,8 +3,8 @@ package com.javarush.task.task08.task0829;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Модернизация ПО
@@ -14,22 +14,48 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> list = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
         while (true) {
+            String city = reader.readLine();
+            if (city.isEmpty()) break;
             String family = reader.readLine();
-            if (family.isEmpty()) {
-                break;
+            if (family.isEmpty()) break;
+            map.put(city, family);
+        }
+        //System.out.println("Ввод города для поиска семьи:");
+        String cityName = reader.readLine();
+
+        for (Map.Entry<String, String> pair :
+                map.entrySet()) {
+            if (pair.getKey().equals(cityName)) {
+                System.out.println(pair.getValue());
             }
-
-            list.add(family);
         }
 
-        // Read the house number
-        int houseNumber = Integer.parseInt(reader.readLine());
+//        List<String> list = new ArrayList<>();
+//        while (true) {
+//            String city = reader.readLine();
+//            String family = reader.readLine();
+//            if (family.isEmpty() || city.isEmpty()) {
+//                break;
+//            }
+//            list.add(city);
+//            list.add(family);
+//        }
+//
+//        // Read the house number
+//        //int houseNumber = Integer.parseInt(reader.readLine());
+//        String cityName = reader.readLine();
+//
+////        if (0 <= houseNumber && houseNumber < list.size()) {
+////            String familyName = list.get(houseNumber);
+////            System.out.println(familyName);
+////        }
+//        for (int i = 0; i < list.size() - 1; i++) {
+//            if (list.get(i).equals(cityName)) {
+//                System.out.println(list.get(i+1));
+//            }
+//        }
 
-        if (0 <= houseNumber && houseNumber < list.size()) {
-            String familyName = list.get(houseNumber);
-            System.out.println(familyName);
-        }
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0830/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task08.task0830;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам Ӏ Java Syntax: 8 уровень, 11 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        //напишите тут ваш код
+        for (int i = array.length - 1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (isGreaterThan(array[j], array[j + 1])) {
+                    String tmp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
